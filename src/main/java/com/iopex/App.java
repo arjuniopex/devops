cdb0a8f61445fb0861445479f1899768c35f94be
Venna Mallikarjuna Reddy <vennamallikarjuna.r@iopex.com>
2019-07-29 22:07:53 +0530

    final commit

---
+++ src/main/java/com/iopex/App.java
@@ -0,0 +1,13 @@
+package com.iopex;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
